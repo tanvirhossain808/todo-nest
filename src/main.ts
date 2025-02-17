a5be1d8c36735a2887085999725698eebe4dfe75
awaismehr <awaismehr001@gmail.com>
2025-02-17 13:58:43 +0600

    add express

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 /* eslint-disable prettier/prettier */
+// /* eslint-disable prettier/prettier */
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors();
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors();
+  await app.init();
 }
+
 bootstrap();
+export default server;
+
