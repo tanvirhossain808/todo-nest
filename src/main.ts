8b1138d322c1b2a7b9abe3afe288ee84d18c5df2
awaismehr <awaismehr001@gmail.com>
2025-02-26 01:38:20 +0600

    remove cookie perser

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
+// import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
+  // app.use(cookieParser());
   app.enableCors();
-  console.log(cookieParser, 'cookiepar');
+  // console.log(cookieParser, 'cookiepar');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
